05ed55b945d1368d612d3022c2ed51d44e2c24c1
Yair Avalos <avalos.consulting.gdl@gmail.com>
2021-05-26 22:47:45 -0500

    Class-Seventeen Practices

--- js/base.js
+++ js/base.js
@@ -1,1 +1,35 @@
-console.log["Hello World"]
+$(document).ready( () => {
+    console.log("Our Document by JQuery it´s Ready")
+
+    $('.content').append(`p`)
+    $('.content p').html("Hola Koder")
+
+    $('.lists ul').append(`
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    `)
+
+    $('.lists ol').prepend(`
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    `)
+
+
+})
+
